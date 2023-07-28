b031af8992a916e0d17824483301418a3caa1e1f
Andrea Zanin <andrea@igloo.ooo>
2023-07-28 14:21:21 +0200

    Deployment with cross-compilation

--- scripts/remote-deploy.sh
+++ scripts/remote-deploy.sh
@@ -1,9 +1,15 @@
+echo "== Building binaries =="
+CC=x86_64-linux-musl-gcc CXX=x86_64-linux-musl-g++ GOARCH=amd64 GOOS=linux CGO_ENABLED=1 \
+    go build -o build/server -ldflags "-linkmode external -extldflags -static" ./cmd/server
+CC=x86_64-linux-musl-gcc CXX=x86_64-linux-musl-g++ GOARCH=amd64 GOOS=linux CGO_ENABLED=1 \
+    go build -o build/os-instrumentation -ldflags "-linkmode external -extldflags -static" ./cmd/instrumentation/os
+
+echo "== Copying binaries to remote server =="
+scp build/* igloo-observability:~/igloo-observability
+scp init/* igloo-observability:~/igloo-observability
+
 ssh -t igloo-observability 'echo "== Connecting to remote server ==" \
-    && cd ~/igloo-observability \
-    && echo "== Fetching latest version from git ==" \
-    && git pull \
-    && echo "== Building the application ==" \
-    && go build -o build/server ./cmd \
     && echo "== Restarting systemctl service ==" \
     && sudo systemctl restart observability-server.service \
+    && sudo systemctl restart observability-os.service \
     && echo "== Deployed successfully =="'
